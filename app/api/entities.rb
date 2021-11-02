309e6bb6639531e5347a6b680336131000fc2f5e
Petru G. Cioată <petru.cioata@gmail.com>
2021-11-02 21:58:49 +0200

    Display County list endpoint

---
+++ app/api/entities.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+class Entities
+  class County < Grape::Entity
+    expose :name
+  end
+end
---
+++ app/api/root_api.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+class RootApi < Grape::API
+  format :json
+
+  rescue_from ActiveRecord::RecordNotFound do
+    error!('Not Found', 404)
+  end
+
+  mount CountiesApi
+
+  add_swagger_documentation(
+    format: :json,
+    base_path: '/api/v1',
+    mount_path: 'docs',
+    info: { title: 'API docs' },
+    models: [],
+    array_use_braces: true,
+    add_root: true
+  )
+end
