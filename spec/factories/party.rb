43751604304bed73ebdad356011687f686205c54
Petru G. Cioată <petru.cioata@gmail.com>
2021-11-06 19:28:46 +0200

    Tiny updates

--- spec/factories/party.rb
+++ spec/factories/party.rb
@@ -4,15 +4,15 @@ FactoryBot.define do
   factory :party do
     name { Faker::Lorem.words(number: 3).join(' ').titleize }
     link { Faker::File.file_name(dir: 'cdep', ext: 'jpg') }
-  end
 
-  trait :with_deputies do
-    transient do
-      deputies_count { 1 }
-    end
+    trait :with_deputies do
+      transient do
+        deputies_count { 1 }
+      end
 
-    after(:create) do |party, evaluator|
-      create_list(:deputy, evaluator.deputies_count, parties: [party])
+      after(:create) do |party, evaluator|
+        create_list(:deputy, evaluator.deputies_count, parties: [party])
+      end
     end
   end
 end
