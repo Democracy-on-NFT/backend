12dc01fb9df440ccba0c2178a0a9ae3843e5dc31
Petru G. Cioată <petru.cioata@gmail.com>
2021-11-18 13:19:44 +0200

    Seed PartiesLegislatures

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,11 +3,13 @@
 require_relative './seeds/electoral_circumscription_seeding'
 require_relative './seeds/legislature_seeding'
 require_relative './seeds/party_seeding'
+require_relative './seeds/party_legislature_seeding'
 
 pp 'Seeding ...'
 
 seed_electoral_circumscriptions
 seed_legislatures
 seed_parties
+seed_parties_legislatures
 
 pp 'Seeding done!'
