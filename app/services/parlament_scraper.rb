afb681c5f3614ab5070197c3d1a8959030f67bff
Petru G. Cioată <petru.cioata@gmail.com>
2021-11-19 16:27:43 +0200

    Add ParlamentScraper service object

--- app/services/parlament_scraper.rb
+++ app/services/parlament_scraper.rb
@@ -2,7 +2,6 @@
 
 require 'bundler/inline'
 
-# ParlamentAppsScraper
 class ParlamentScraper < Kimurai::Base
   @name = 'parlament_spider'
   @engine = :mechanize
@@ -15,7 +14,7 @@ class ParlamentScraper < Kimurai::Base
     @base_uri = 'http://www.cdep.ro'
     response = browser.current_response
 
-    response.css('div.grup-parlamentar-list table tbody tr').first(1).each do |line|
+    response.css('div.grup-parlamentar-list table tbody tr').first(8).last(2).each do |line|
       href = line.css('td')[1].css('a').attr('href')
       browser.visit(@base_uri + href)
       app_response = browser.current_response
@@ -36,9 +35,63 @@ class ParlamentScraper < Kimurai::Base
 
     item[:name] = response&.css('div.boxTitle h1')&.text&.squish
     item[:picture_url] = @base_uri + response&.css('div.profile-pic-dep a img')&.attr('src')&.text&.squish
+    item[:email] = response&.css('span.mailInfo')&.text&.squish
+    item[:party] = response&.css('div.boxDep')[1]&.css('table tr td')&.text&.squish
+    # periods = response&.css('div.profile-dep h3')&.text&.squish.split.last.split("-")
+    # item[:legislature] = {
+    #   start_date: periods[0],
+    #   end_date: periods[1]
+    # }
+    item[:electoral_circumscription] = response&.css('div.boxDep')[0]&.text&.squish&.split[6]&.split(".").last
+
+    addresses(response&.css('div.boxDep')[-1], item)
+    parties(response&.css('div.boxDep')[2]&.css('table'), item)
+
+    deputy_legislature = StoreDeputyInfo.new(item).call
+
     activity(response&.css('div.boxDep')[-2]&.css('table'), item)
 
-    save_to 'result_nou.json', item, format: :pretty_json, position: false
+    StoreDeputyActivity.new(data: item, deputy_legislature: deputy_legislature).call
+    # save_to "result_nou.json", item, format: :pretty_json, position: false
+  end
+
+  def parties(table, item)
+    array = []
+    table&.css('tr').each do |line|
+      name = line&.css('td')[0]&.text&.squish&.split("Grupul parlamentar ").last
+      name = name[3..] if name[..2] == 'al '
+      name = acronym(name)
+      start_date = '2020-12-20'
+      end_date = nil
+
+      text = line&.css('td')&.last&.text&.squish
+      case text
+      when /din/
+        start_date = format_date(text)
+      when /până în/
+        end_date = format_date(text)
+      end
+
+      array << {
+        party: name,
+        start_date: start_date,
+        end_date: end_date
+      }
+    end
+    item[:parties] = array
+  end
+
+  def addresses(box, item)
+    array = []
+    list = box&.css('ol li')
+    unless list.empty?
+      list.map { |li| array << li&.text&.squish }
+    else
+      box&.css('p').map do |p|
+        p&.text&.split("\n").map { |address| array << address.squish[3..] }
+      end
+    end
+    item[:addresses] = array
   end
 
   def activity(table, item)
@@ -56,19 +109,21 @@ class ParlamentScraper < Kimurai::Base
         item[:speaking_count] = value
         last_date = nil
         response&.css('div.grup-parlamentar-list table tbody tr').each do |li|
-          val = li&.css('td table.innertable tbody tr td')[0]&.text&.squish
+          val = unless li&.css('td table.innertable tbody tr td b').empty?
+                  li&.css('td table.innertable tbody tr td')[0]&.text&.squish
+                else
+                  nil
+                end
           if val
-            last_date = parse_date(date(val))
+            last_date = date(val)
           else
             icon_src = li&.css('td a img')&.attr('src')&.text&.squish
-            if icon_src == '/img/icon_go2.gif'
-              array << {
-                title: {
-                  first: li&.css('td')&.text&.squish
-                },
-                date: last_date
-              }
-            end
+            array << {
+              title: {
+                first: li&.css('td')&.text&.squish
+              },
+              date: last_date
+            } if icon_src == '/img/icon_go2.gif'
             array[-1][:title][:second] = li&.css('td')&.text&.squish if array[-1] && icon_src == '/img/icon_go1.gif'
           end
         end
@@ -100,9 +155,11 @@ class ParlamentScraper < Kimurai::Base
         response&.css('div.grup-parlamentar-list table tbody tr').each do |li|
           val = li&.css('td')[1]&.text&.squish&.split('/')
           i = val[1].index(' ')
+          kind_number = val[0]&.squish.split
           array << {
-            title: val[1][i + 1..]&.squish,
-            number: val[0]&.squish,
+            title: val[1][i+1..]&.squish,
+            kind: kind_number[0],
+            number: kind_number[1],
             date: parse_date(val[1][...i]&.squish)
           }
         end
@@ -114,7 +171,8 @@ class ParlamentScraper < Kimurai::Base
           array << {
             title: li&.css('td')[1]&.text&.squish,
             number: val[0]&.squish,
-            date: parse_date(val[1]&.squish)
+            date: parse_date(val[1]&.squish),
+            status: li&.css('td')[4]&.text&.squish&.downcase
           }
         end
         item[:motions_signed] = array
@@ -143,7 +201,48 @@ class ParlamentScraper < Kimurai::Base
     }
     text = text.split.last(3)
     text[1] = h[text[1].to_sym]
-    text.join('-')
+    parse_date(text.join('-'))
+  end
+
+  def format_date(text)
+    h = {
+      ian: 1,
+      feb: 2,
+      mar: 3,
+      apr: 4,
+      mai: 5,
+      iun: 6,
+      iul: 7,
+      aug: 8,
+      sep: 9,
+      oct: 10,
+      noi: 11,
+      dec: 12
+    }
+    text = text.split.last(2)
+    text.unshift(1)
+    text[1] = h[text[1][..2].to_sym]
+    parse_date(text.join('-'))
+  end
+
+  def acronym(name)
+    case name.downcase
+      when /social democrat/
+        name = 'PSD'
+      when /naţional liberal/
+        name = 'PNL'
+      when /salvaţi românia/
+        name = 'USR'
+      when /AUR/
+        name = 'AUR'
+      when /democrate maghiare din românia/
+        name = 'UDMR'
+      when /minorităţi/
+        name = 'MIN'
+      when /neafiliaţi/
+        name = 'NA'
+    end
+    name
   end
 end
 
