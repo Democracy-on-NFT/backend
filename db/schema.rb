5d00dcfe78779d0d42b1ee655f14c29442604778
Petru G. Cioată <petru.cioata@gmail.com>
2021-11-19 15:31:10 +0200

    Update status type for SignedMotion

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_18_104032) do
+ActiveRecord::Schema.define(version: 2021_11_19_094931) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -120,7 +120,7 @@ ActiveRecord::Schema.define(version: 2021_11_18_104032) do
     t.text "title"
     t.string "number"
     t.date "date"
-    t.integer "status", limit: 2
+    t.string "status"
     t.bigint "deputy_legislature_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
