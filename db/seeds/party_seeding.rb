10744528880e7fdb1b3f8020cdc106a6915b469f
Petru G. Cioată <petru.cioata@gmail.com>
2021-11-28 22:56:54 +0200

    Update party's seeds

--- db/seeds/party_seeding.rb
+++ db/seeds/party_seeding.rb
@@ -10,12 +10,14 @@ def seed_parties
   CSV.foreach(filename, headers: true) do |row|
     party_information = {
       abbreviation: row[0],
-      name: row[1],
-      link: row[2]
+      name: row[1]
     }
-    Party
-      .where(party_information)
-      .first_or_create!(party_information)
+    # https://newbedev.com/rails-create-or-update-magic
+    Party.where(party_information).first_or_initialize(party_information).tap do |party|
+      party.link = row[2]
+      party.logo = row[3]
+      party.save
+    end
   end
 
   pp "Seeding Parties from #{filename} done."
