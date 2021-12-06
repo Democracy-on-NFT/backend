661b6ea3b69abda60a29fb10800fb4dd2ac7373a
Petru G. Cioată <petru.cioata@gmail.com>
2021-12-07 01:02:09 +0200

    Add NotifierMailer base structure

---
+++ app/mailers/notifier_mailer.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+class NotifierMailer < ApplicationMailer
+  def monthly_notification(electoral_circumscription_id)
+    receivers_list = Notification
+      .where(electoral_circumscription_id: electoral_circumscription_id)
+      .map(&:email)
+      .uniq
+
+    ec = ElectoralCircumscription.find(electoral_circumscription_id)
+    activity_month = "#{Date.current.month - 1} - #{Date.current.year}"
+    subject = "Activitate parlamentarilor din circumscripția #{ec.number} în luna #{activity_month}"
+
+    # TODO: update the logic on this part
+    attachments['ActivitateParlamentari.pdf'] = WickedPdf.new.pdf_from_string(
+      render_to_string(
+        pdf: 'monthly_report',
+        template: 'monthly_report.html.erb',
+        locals: { @ec => ec }
+      )
+    )
+    mail(
+      bcc: receivers_list,
+      subject: subject
+    )
+  end
+end
